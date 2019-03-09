dbd310b6b76daf6a5bde50dbf45531b6e2808b22
youyo <1003ni2@gmail.com>
2019-03-10 03:04:47 +0900

    update

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from setuptools import setup
 from pip._internal.req import parse_requirements
-import subprocess
 
 install_reqs = parse_requirements('requirements.txt', session='hack')
 requires = [str(ir.req) for ir in install_reqs]
-lamblackbox_version_bytes = subprocess.run(
-    ['git', 'describe', '--tags', '--abbrev=0'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
 setup(
     name='lamblackbox',
-    version=lamblackbox_version_bytes,
     description='This is a AWS Lambda function logging library.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    setup_requires=['setuptools_scm'],
     url='https://github.com/youyo/lamblackbox',
     author='youyo',
     author_email='1003ni2@gmail.com',
